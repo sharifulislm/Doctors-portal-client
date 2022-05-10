48aab81889509e91c42bd9289c56adc15b3173f4
sharifulislm <shariful.islam.coder@gmail.com>
2022-05-10 20:43:52 +0300

    added benner

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import chair from '../../assets/images/chair.png';
+import './Home.css';
 
 const Banner = () => {
     return (
-        <div class="hero min-h-screen bg-base-200">
+        <div class="hero min-h-screen bg-base-200 benner-box">
         <div class="hero-content flex-col lg:flex-row-reverse">
           <img src={chair} class="max-w-sm rounded-lg shadow-2xl" />
           <div>
-            <h1 class="text-5xl font-bold">Box Office News!</h1>
-            <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-            <button class="btn btn-primary">Get Started</button>
+            <h1 class="text-5xl font-bold">Your New Smile Starts Here</h1>
+            <p class="py-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the</p>
+            <button class="btn btn-primary uppercase bg-gradient-to-r from-secondary to-primary">Get Started</button>
           </div>
         </div>
       </div>
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,7 +3,7 @@ import Banner from './Banner';
 
 const Home = () => {
     return (
-        <div>
+        <div className='px-12 '>
            <Banner></Banner>
             
         </div>
