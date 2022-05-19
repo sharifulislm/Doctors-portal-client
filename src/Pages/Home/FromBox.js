62728fd98e47692c04c9c4716824e33ba53aebb8
sharifulislm <shariful.islam.coder@gmail.com>
2022-05-19 22:59:43 +0300

    solved soeme error

--- src/Pages/Home/FromBox.js
+++ src/Pages/Home/FromBox.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import appointment from '../../assets/images/appointment.png';
 import emailjs from '@emailjs/browser';
+import { toast } from 'react-toastify';
 
 
 const FromBox = () => {
@@ -10,9 +11,13 @@ const FromBox = () => {
     emailjs.sendForm('service_w5ckfr1', 'template_4sllcq9', e.target, 'HmDKt3gAaokdpNhzA')
       .then((result) => {
           console.log(result.text);
+          toast('Successfully sent your email')
       }, (error) => {
           console.log(error.text);
+          toast('Failed your email Try again')
+          
       });
+      e.target.reset();
   };
 
 
@@ -31,7 +36,7 @@ const FromBox = () => {
       </div>
    <input type="text" placeholder="name" name='name' className="input w-full max-w-xs w-full max-w-xs text-black mb-5" />
    <input type="email" placeholder="email" name='email' className="input w-full max-w-xs w-full max-w-xs text-black mb-5" />
-   <textarea type="text" placeholder="Your message" name='message' className="input w-full max-w-xs w-full max-w-xs text-black mb-5  " />
+   <textarea type="text" placeholder="Your message" name='message' className="input w-full max-w-xs w-full max-w-xs h-32 text-black mb-5  " />
 
 <br></br>
 <input className='btn btn-secondary' type="submit" value='submit'/>
